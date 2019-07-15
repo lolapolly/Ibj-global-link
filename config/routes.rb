7654d7a6779b3fdcc23e4c821e61ae22c8b57f75
omololaibojo <lola.polly@yahoo.it>
2019-07-15 23:46:45 +0100

    items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/new'
+  get 'items/create'
+  get 'items/edit'
+  get 'items/show'
+  get 'items/update'
+  get 'items/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items do
+    resources :purchases, only: [:new, :create, :show]
+  end
 end
