5a748038469abc56d57b9836ea2fc2ee880d7ccd
omololaibojo <lola.polly@yahoo.it>
2019-07-29 16:41:55 +0100

    Installed pundit gem

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 ruby '2.5.3'
 
 gem 'bootsnap', require: false
+gem 'pundit'
 gem 'devise'
+
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/create'
-  get 'items/edit'
-  get 'items/show'
-  get 'items/update'
-  get 'items/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
