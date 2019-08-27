4bd05fc471cdacfdc53a25bef1f55dc930789cc7
omololaibojo <lola.polly@yahoo.it>
2019-08-28 00:03:52 +0100

    Nokogiri update

---
+++ app/assets/stylesheets/components/ex16.rb
@@ -0,0 +1,37 @@
+filename = ARVG.first
+puts "We're going to erase #{filename}"
+puts "if you don't want that, hit CTRL-C (^C)."
+puts "if you don't want that, hit RETURN"
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.getschomp
+print "line 3: "
+line3 = $stdin.getschomp
+
+puts "I'm going to write these to the file"
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+puts "And finally, we close it."
+target.close
+
+
+
+
+
